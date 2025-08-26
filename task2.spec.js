8273cff9b39cdb4b39875959cbf6cafe1130cc15
Your Name <cutdomi@gmail.com>
2025-08-26 13:30:50 +0200

    Changes made, added .env

--- task2.spec.js
+++ task2.spec.js
@@ -1,6 +1,15 @@
 const { test, expect } = require('@playwright/test');
+require('dotenv').config();
 
 test.describe.configure({ mode: 'serial' });
+
+async function performLogin(page, username, password) {
+    await page.fill('#username', username);
+    await page.fill('#password', password);
+    await page.click('#submit');
+}
+
+if (!PASSWORD) throw new Error('LOGIN_PASS is not set');
 
 test.describe('Login Tests', () => {
 
@@ -9,21 +18,16 @@ test.describe('Login Tests', () => {
     });
 
     test('Successful login', async ({ page }) => {
-        await page.fill('#username', 'student');
-        await page.fill('#password', 'Password123');
-        await page.click('#submit');
-
+        await performLogin(page, USERNAME, PASSWORD);
         await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
         await expect(page.locator('h1')).toHaveText('Logged In Successfully');
     });
 
-    test('Invalid Password', async ({ page }) => {
-        await page.fill('#username', 'student');
-        await page.fill('#password', 'wrongPassword');
-        await page.click('#submit');
-
+    test('Invalid password error', async ({ page }) => {
+        await performLogin(page, USERNAME, WRONG_PASSWORD);
         const errorMessage = page.locator('#error');
         await expect(errorMessage).toBeVisible();
         await expect(errorMessage).toContainText('Your password is invalid!');
     });
+
 });
